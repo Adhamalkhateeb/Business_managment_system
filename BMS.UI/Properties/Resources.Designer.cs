1877d2114567d547c3bf004fcd7e1b304b2e26c0
Beshir <hsnnasser10@gmail.com>
2025-07-14 14:21:25 +0300

    Adding User Control For Lists With initial Settings

--- BMS.UI/Properties/Resources.Designer.cs
+++ BMS.UI/Properties/Resources.Designer.cs
@@ -383,6 +383,16 @@ namespace BMS.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap List {
+            get {
+                object obj = ResourceManager.GetObject("List", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap mall {
             get {
                 object obj = ResourceManager.GetObject("mall", resourceCulture);
